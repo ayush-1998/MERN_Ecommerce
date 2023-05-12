0bc39514a75caf029216c5e196c81ce108147af7
Ayush Pegwar <ayushpegwar29081998@gmail.com>
2023-05-12 12:56:42 +0530

    working on user route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,49 @@
+const ErrorHander = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const User = require("../model/userModel");
+const sendToken = require("../utils/jwtToken");
+
+// Register a User
+
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a sample id",
+      url: "profileurl",
+    },
+  });
+
+    sendToken(user, 201, res);
+});
+
+//Login User
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // checking if user has given password and email both
+
+  if (!email || !password) {
+    return next(new ErrorHander("Please Enter Email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHander("Invalid email or password", 401));
+  }
+
+
+    sendToken(user, 200, res);
+});
